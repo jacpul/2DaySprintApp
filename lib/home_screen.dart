bea3d21d0055f85e598e858f9f8099c17d8ee0bb
Jordan Edel <edel0071@d.umn.edu>
2023-12-05 16:23:07 -0600

    linked login, home, and vidoes screens

---
+++ lib/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:final_project/videos_screen.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _Home createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Builder(
+        builder: (context) =>
+            Scaffold (
+              appBar: AppBar(
+                title: Text("Home"),
+                backgroundColor: Colors.deepOrange,
+              ),
+              body: Container(
+                alignment: Alignment.center,
+                child: Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.blueAccent
+                      ),
+                      child: Text('Videos', style: const TextStyle(fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white),
+                          textAlign: TextAlign.center),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                          return VideoResource();
+                        }));
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+      ),
+    );
+  }
+}
