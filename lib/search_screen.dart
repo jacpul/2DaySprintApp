c34c7f0a6f981377a5ebaa83f719f0dc31c37dfd
ross0738 <ross0738@d.umn.edu>
2023-12-06 16:50:20 -0600

    Added some basic UI for the search screen

--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -11,12 +11,25 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreen extends State<SearchScreen> {
+  // Disposes text editing controllers used for the search
+  @override
+  void dispose() {
+    _searchBarController.dispose();
+
+    super.dispose();
+  }
+
+  // Used to determine how we are searching
+  String _searchMethod = "Title";
+  String _searchBarText = "";
+
+  final _searchBarController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Search"),
+          title: const Text("Search"),
           centerTitle: true,
           backgroundColor: Colors.deepOrangeAccent,
           actions: [
@@ -30,6 +43,74 @@ class _SearchScreen extends State<SearchScreen> {
                 }
             )
           ]
+      ),
+      body: Container(
+        color: Colors.orangeAccent.shade100,
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Radio<String>(
+                  activeColor: Colors.blue,
+                  value: "Title",
+                  groupValue: _searchMethod,
+                  onChanged: (value) {
+                    setState(() {
+                        _searchMethod = value!;
+                    });
+                  }),
+                const Text(
+                  'Title',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                Radio<String>(
+                    activeColor: Colors.blue,
+                    value: "Author",
+                    groupValue: _searchMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _searchMethod = value!;
+                      });
+                    }),
+                const Text(
+                  'Author',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                Radio<String>(
+                    activeColor: Colors.blue,
+                    value: "ISBN",
+                    groupValue: _searchMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _searchMethod = value!;
+                      });
+                    }),
+                const Text(
+                  'ISBN Number',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ],
+            ),
+            TextFormField(
+              controller: _searchBarController,
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.next,
+              onChanged: (value) {
+                setState(() {
+                  _searchBarText = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter Details Here',
+                labelText: _searchMethod,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
