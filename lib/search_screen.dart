dd258e3432f5c67cef8d992a385f11ff9e418560
Gabe Findell <finde040@umn.edu>
2023-12-06 15:14:12 -0600

    removed unused login dart file

--- lib/search_screen.dart
+++ lib/search_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'home_screen.dart';
-import 'login_screen.dart';
+import 'login.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
